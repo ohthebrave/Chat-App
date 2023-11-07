c4519e2ebcf934ab4161e35c7b3b45517257df29
George <georgejoj28@gmail.com>
2023-11-07 13:17:21 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Welcome from "./components/Welcome"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Welcome/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,20 @@
+import {AiOutlineMessage} from "react-icons/ai";
+import phone_img from "../assets/vd-call.webp"
+
+const Welcome = () => {
+    return ( 
+        <section className="container" >
+            <div className="d-flex flex-row mb-3">
+                <div className="p-5 align-items-center" >
+                    <h1> <AiOutlineMessage/> Hang out anytime, anywhere and message privately.</h1>
+                    <p className="p-5 fw-lighter lh-sm text-success-emphasis">Simple, reliable, private messaging and calling for free*, available all over the world.</p>
+                </div>
+                <div>
+                    <img className="img" src={phone_img} alt="phone" />
+                </div>
+            </div>
+        </section>
+     );
+}
+ 
+export default Welcome;
